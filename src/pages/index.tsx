a9005876567715b1b70a31b3e8e89d2e0460dc74
igorbrands <igorcantelmo@gmail.com>
2020-11-08 23:10:07 -0300

    uploading local server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,32 @@
 import { Title } from '../styles/pages/Home';
 
+interface IProduct {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
   return (
     <div>
-      <Title>Hello World!</Title>
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map((recommendedProduct) => {
+            return (
+              <li key={recommendedProduct.id}>{recommendedProduct.title}</li>
+            );
+          })}
+        </ul>
+      </section>
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  fetch('http://localhost:3333/recommended').then((response) => {
+    response.json().then((data) => {
+      setRecommendedProducts(data);
+    });
+  });
+};
